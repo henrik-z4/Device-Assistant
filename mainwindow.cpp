83921b733d88d71e8f8267e97cecadcb79228bc9
Henrik <muradyango@student.bmstu.ru>
2023-12-03 02:30:32 +0300

    Получение характеристик ПК

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSysInfo>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    // Получение информации о характеристиках ПК
+    QString osInfo = "Операционная система: " + QSysInfo::prettyProductName();
+    QString cpuInfo = "Архитектура: " + QSysInfo::currentCpuArchitecture();
+    QString ramInfo = "Оперативная память: " + QString::number(QSysInfo::availableVirtualMemory() / (1024 * 1024)) + " Мегабайт";
+
+    // Вывод характеристик в консоль (в будущем в GUI)
+    qDebug() << osInfo;
+    qDebug() << cpuInfo;
+    qDebug() << ramInfo;
 }
 
 MainWindow::~MainWindow() {
